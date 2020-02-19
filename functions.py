57afab08705c95d0f0f20e97ebce8030be4999a3
ryazanov-s-a <61159872+ryazanov-s-a@users.noreply.github.com>
2020-02-19 22:20:16 +0300

    [STEP-2] Add func sum_in_string

--- functions.py
+++ functions.py
@@ -7,3 +7,17 @@ def triangle(x):
         print('#' * b)
         b += 1
     return triangle
+
+
+def sum_in_string(x):
+    """
+    На вход функции подаём параметр х - произвольная строка.
+    На выходе получаем сумму всех цифр
+    """
+    s = 0
+    for i in x:
+        try:
+            s += int(i)
+        except ValueError:
+            continue
+    print(s)
